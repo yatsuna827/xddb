7908c9ea6440a20ec03984b7cd11eb02c3705840
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-09-20 01:30:47 +0900

    LCGにadvance(n)を実装

---
+++ src/tests/test_lcg.py
@@ -0,0 +1,35 @@
+import unittest
+
+from xddb.lcg import LCG
+
+
+class TestLCG(unittest.TestCase):
+    def test_advance(self):
+        expected = [
+            0x41A8D6F9,
+            0x99313DD8,
+            0x0288453B,
+            0xFAA27B12,
+            0xAA25F58D,
+            0xF795321C,
+            0x510A786F,
+            0x3E51B176,
+            0x0CB6E261,
+            0x1546BBA0,
+        ]
+        lcg = LCG(0xC0FFEE)
+        self.assertEqual(expected, [lcg.adv() for _ in range(0, 10)])
+
+    def test_jump(self):
+        lcg = LCG(0xBEEF)
+        self.assertEqual(0x5ACCB204, lcg.adv(0x827827))
+
+    def test_back(self):
+        seed = 0xBADFACE
+        lcg = LCG(seed)
+        self.assertNotEqual(seed, lcg.adv())
+        self.assertEqual(seed, lcg.back())
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/xddb/lcg.py
+++ src/xddb/lcg.py
@@ -1,4 +1,16 @@
-from typing import Optional
+from typing import List, Optional, Tuple
+
+
+def _precalc(a: int, b: int) -> List[Tuple[int, int]]:
+    cache = []
+    for i in range(0, 32):
+        cache.append((a, b))
+        a, b = a * a & 0xFFFFFFFFF, (b * (1 + a)) & 0xFFFFFFFF
+    return cache
+
+
+_doubling_f = _precalc(0x343FD, 0x269EC3)
+_doubling_f_inv = _precalc(0xB9B33155, 0xA170F641)
 
 
 class LCG(object):
@@ -7,13 +19,26 @@ class LCG(object):
     def __init__(self, seed: int) -> None:
         self.seed = seed
 
-    def adv(self) -> int:
-        self.seed = (self.seed * 0x343FD + 0x269EC3) & 0xFFFFFFFF
+    def _jump(self, n: int, mem: List[Tuple[int, int]]) -> int:
+        for i in range(0, 32):
+            if n & (1 << i):
+                a, b = mem[i]
+                self.seed = (self.seed * a + b) & 0xFFFFFFFF
         return self.seed
 
-    def back(self) -> int:
-        self.seed = (self.seed * 0xB9B33155 + 0xA170F641) & 0xFFFFFFFF
-        return self.seed
+    def adv(self, n: Optional[int] = None) -> int:
+        if n is None:
+            self.seed = (self.seed * 0x343FD + 0x269EC3) & 0xFFFFFFFF
+            return self.seed
+        else:
+            return self._jump(n, _doubling_f)
+
+    def back(self, n: Optional[int] = None) -> int:
+        if n is None:
+            self.seed = (self.seed * 0xB9B33155 + 0xA170F641) & 0xFFFFFFFF
+            return self.seed
+        else:
+            return self._jump(n, _doubling_f_inv)
 
     def rand(self, m: Optional[int] = None) -> int:
         rand = self.adv() >> 16
